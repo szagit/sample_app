ea1b06406dc0a7edfe5820a81bb1883606a88663
Saul Farber <saulfarber@gmail.com>
2012-10-24 00:03:54 -0500

    Add user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 SampleApp::Application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy] #no UPDATE action
+  resources :microposts, only: [:create, :destroy]
 
   root to: 'static_pages#home'
 
   match '/signup',  to: 'users#new'
   match '/signin',  to: 'sessions#new'
-  match '/signout', to: 'sessions#destroy'
+  match '/signout', to: 'sessions#destroy', via: :delete #what does "via delete" do?
   
 
   match '/help',    to: 'static_pages#help'
